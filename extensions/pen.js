6b058be78085f3c0a53e6ddfdf3c262037bc794b
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-14 13:26:44 -0400

    Add pen extension with basic commands
    Register 'pen' extension with commands for pen control.

---
+++ extensions/pen.js
@@ -0,0 +1,12 @@
+DunbunMod.extensions.register({
+  id:"pen",
+  name:"Pen",
+  blocks:[
+    { opcode:"penDown", blockType:"command", text:"pen down" }
+  ],
+      { opcode:"penUp", blockType:"command", text:"pen up" }
+  ],
+      { opcode:"penRight", blockType:"command", text:"pen right" }
+  ],
+      { opcode:"penLeft", blockType:"command", text:"pen left" }
+  ],
