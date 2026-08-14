45717698a0a3a90187a542273af8b1ca6070b858
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-14 13:30:06 -0400

    Add video sensing extension with motion blocks

---
+++ extensions/video.js
@@ -0,0 +1,14 @@
+DunbunMod.extensions.register({
+  id:"video",
+  name:"Video Sensing",
+  blocks:[
+    { opcode:"motion", blockType:"reporter", text:"video motion" }
+  ],
+  { opcode:"motion", blockType:"reporter", text:"go to nose" }
+  ],
+  { opcode:"motion", blockType:"reporter", text:"go to ear" }
+  ]
+{ opcode:"motion", blockType:"reporter", text:"go to eye" }
+  ],
+  motion(){ return 0; }
+});
